733e4ac4612c34f4fba0c1d64457123396099a19
gitdew <krisztian.harmat@gmail.com>
2024-09-27 20:15:58 +0200

    initial commit

---
+++ backend/src/main/java/com/github/gitdew/boatapp/BoatappApplication.java
@@ -0,0 +1,13 @@
+package com.github.gitdew.boatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/gitdew/boatapp/BoatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gitdew.boatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
