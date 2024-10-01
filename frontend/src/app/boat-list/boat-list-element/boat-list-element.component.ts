da926a2253d9196b4c5181a4053b20b6afcd956b
gitdew <krisztian.harmat@gmail.com>
2024-10-01 21:49:19 +0200

    Replace rudimentary delete button with a trash can icon

--- frontend/src/app/boat-list/boat-list-element/boat-list-element.component.ts
+++ frontend/src/app/boat-list/boat-list-element/boat-list-element.component.ts
@@ -1,13 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Boat } from '../models/Boat';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { BoatsService } from '../../boats.service';
 
 @Component({
   selector: 'app-boat-list-element',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './boat-list-element.component.html',
   styleUrl: './boat-list-element.component.css',
 })
 export class BoatListElementComponent {
+  faTrashCan = faTrashCan;
+
   @Input() boat!: Boat;
+
+  @Output() deleted = new EventEmitter();
+
+  constructor(private boatsService: BoatsService) {}
+
+  onDelete(): void {
+    this.boatsService.deleteBoat(this.boat.id).subscribe(() => {
+      this.deleted.emit();
+    });
+  }
 }
--- frontend/src/app/boat-list/boat-list.component.ts
+++ frontend/src/app/boat-list/boat-list.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { BoatDetailComponent } from './boat-detail/boat-detail.component';
 import { BoatsService } from '../boats.service';
 import { Boat } from './models/Boat';
-import { AsyncPipe } from '@angular/common';
 import { BoatListElementComponent } from './boat-list-element/boat-list-element.component';
 
 @Component({
   selector: 'app-boat-list',
   standalone: true,
-  imports: [BoatDetailComponent, AsyncPipe, BoatListElementComponent],
+  imports: [BoatListElementComponent],
   templateUrl: './boat-list.component.html',
   styleUrl: './boat-list.component.css',
 })
