8643d19a7fc2f4ebfe3a4cb17ec2984e6ec6898c
gitdew <krisztian.harmat@gmail.com>
2024-10-01 23:00:20 +0200

    Better mock data

--- backend/src/main/java/com/github/gitdew/boatapp/DataLoader.java
+++ backend/src/main/java/com/github/gitdew/boatapp/DataLoader.java
@@ -12,10 +12,12 @@ public class DataLoader implements ApplicationRunner {
   private final BoatRepository boatRepository;
 
   public void run(ApplicationArguments args) {
-    boatRepository.save(new Boat(1L, "Backend Boat #1", "A boat from the database."));
+    boatRepository.save(new Boat(1L, "Backend Boat #1", ""));
     boatRepository.save(new Boat(2L, "Backend Boat #2", "Another boat from the database."));
     boatRepository.save(new Boat(3L, "Backend Boat #3", "Another boat from the database."));
-    boatRepository.save(new Boat(4L, "Backend Boat #4", "Another boat from the database."));
+    boatRepository.save(new Boat(4L,
+        "A boat with a really long name and long description. Really really long. Do things overflow? Is it still nice?",
+        "This is a really long description of a boat. Let's see how it appears in the UI. Do things overflow? Is it still nice? This is a really long description of a boat. Let's see how it appears in the UI. Do things overflow? Is it still nice?"));
     boatRepository.save(new Boat(5L, "Backend Boat #5", "Another boat from the database."));
     boatRepository.save(new Boat(6L, "Backend Boat #6", "Another boat from the database."));
     boatRepository.save(new Boat(7L, "Backend Boat #7", "Another boat from the database."));
